087e3a4443eeeffbe8292f780bfb7ddb4a49bb46
santiago5087 <santiago5087@hotmail.com>
2026-04-21 09:08:17 -0500

    feat: Confirm dialog component

--- src/app/presentation/components/molecules/index.ts
+++ src/app/presentation/components/molecules/index.ts
@@ -1,3 +1,4 @@
 export * from './action-card/action-card.component';
 export * from './fund-card/fund-card.component';
 export * from './subscription-form/subscription-form.component';
+export * from './confirm-dialog/confirm-dialog.component';
