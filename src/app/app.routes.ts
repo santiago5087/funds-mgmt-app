4268e66bfa50e29f88a195e8e91e42ef1fd00281
santiago5087 <santiago5087@hotmail.com>
2026-04-21 00:37:12 -0500

    feat: Funds page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from '@presentation/pages';
+import { FundsPageComponent, HomePageComponent } from '@presentation/pages';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomePageComponent,
   },
-{
+  {
+    path: 'funds',
+    component: FundsPageComponent,
+  },
+  {
     path: '**',
     redirectTo: '',
   },
--- src/app/presentation/pages/index.ts
+++ src/app/presentation/pages/index.ts
@@ -1,1 +1,2 @@
 export * from './home/home-page.component';
+export * from './funds/funds-page.component';
