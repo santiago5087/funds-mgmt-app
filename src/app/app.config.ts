40164e74002c25f7735949a945d366c499c24695
santiago5087 <santiago5087@hotmail.com>
2026-04-20 23:06:41 -0500

    feat: state management with NGXS

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { DATA_PROVIDERS } from '@data/data.providers';
+import { provideStore } from '@ngxs/store';
+import { FundsState } from '@core/store/states/funds.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideStore([FundsState]),
     ...DATA_PROVIDERS,
   ]
 };
