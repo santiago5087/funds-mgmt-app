57abc5a821ea06b26ae8f006765f845065ede026
santiago5087 <santiago5087@hotmail.com>
2026-04-21 09:27:39 -0500

    feat: transactions table component

--- src/app/presentation/components/organisms/index.ts
+++ src/app/presentation/components/organisms/index.ts
@@ -1,3 +1,4 @@
 export * from './header/header.component';
 export * from './funds-grid/funds-grid.component';
 export * from './footer/footer.component';
+export * from './transactions-table/transactions-table.component';
